1d596972a008eb3c33c05a6ec63710eda42c35fe
boumnadeMY <boumyass4@gmail.com>
2023-09-19 16:11:06 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 void reset_to_98(int *n) {
-    *n = 98; // Update the value pointed to by n to 98
+    *n = 98; /* Update the value pointed to by n to 98 */
 }
 
 int main() {
-    int number = 42;
-    printf("Before reset_to_98: %d\n", number);
+    int number = 402;
+    printf("n = %d\n", number);
 
-    // Call the function to reset the value to 98
     reset_to_98(&number);
 
-    printf("After reset_to_98: %d\n", number);
-
-    return 0;
+    printf("n = %d\n", number);
 }
