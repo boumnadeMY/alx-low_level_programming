4f2a86844f70e32dec1f98029a6127cd75648c66
boumnadeMY <boumyass4@gmail.com>
2023-09-21 15:16:08 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include “main.h”
+
+Void positive_or_negative(int I)
+
+{
+        if (I > 0)
+                printf("%d is positive\n", i);
+        else if (I == 0)
+                printf("%d is zero\n", i);
+        else
+                printf("%d is negative\n", i);
+}
